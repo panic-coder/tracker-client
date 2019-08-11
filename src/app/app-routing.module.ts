4b656ca246fccb3587b5bceb8d4b3fac41356db5
panic-coder <androidshubham@gmail.com>
2019-08-11 09:32:22 +0530

    add auth and http service

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AuthGuardService as AuthGuard } from './auth/auth-guard.service';
 import { TrackerComponent } from './components/tracker/tracker.component';
 import { LoginComponent } from './components/login/login.component';
 
 const routes: Routes = [
-  { path: '', component: TrackerComponent },
+  { path: 'tracker', component: TrackerComponent, canActivate: [AuthGuard] },
   { path: 'login', component: LoginComponent },
-  // { path: '', redirectTo: 'login', pathMatch: 'full' },
-  // { path: '**', redirectTo: 'login', pathMatch: 'full' }
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: '**', redirectTo: 'login', pathMatch: 'full' }
 ];
 
 @NgModule({
